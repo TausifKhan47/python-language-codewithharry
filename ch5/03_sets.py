22e78e910fe780e6928fa985fea639ebe17f4163
TausifKhan <tausif.n.n.73n@gmail.com>
2025-11-04 13:06:38 +0530

    Add files via upload

---
+++ ch5/03_sets.py
@@ -0,0 +1,5 @@
+s={1,2,32,5,5,76}
+
+e=set()  #Dont use s ={} as it will create the empty dictionary 
+
+print(s)
---
+++ ch5/04_sets_methods.py
@@ -0,0 +1,7 @@
+s={34,56,78,33,4,5,4,"Harry"}
+print(s,type(s))
+
+s.add(44)
+s.remove(34)
+s.clear()
+print(s,type(s))
